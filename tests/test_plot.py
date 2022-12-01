cae18b8c0b1a42068d4512f696014d46a886b4a2
Andrew <mail@awmacpherson.com>
2022-12-01 12:36:44 +0900

    Added matplotlib utility functions.

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -1,7 +1,7 @@
 from tope.plot import *
 import os, pytest
 
-def test_save_figs_to_dir(tmp_path, tmp_path_factory):
+def test_save_figs_to_dir(tmp_path):
     fig, _ = plt.subplots()
 
     assert os.path.exists(tmp_path)
@@ -38,5 +38,77 @@ def test_save_figs_to_dir(tmp_path, tmp_path_factory):
     )
     assert os.path.exists("b/c/d.png")
 
-def test_save_figs_to_zip():
-    pass
+def test_save_figs_to_zip(tmp_path):
+    fig, _ = plt.subplots()
+    os.chdir(tmp_path)
+
+    # desired behaviour:
+    # archive does not exist but parent exists YES
+    # neither archive nor parent(s) exists. YES
+    # archive exists as ordinary file YES if overwrite else NO
+    # archive exists as directory YES WITH WARNING if overwrite else NO
+
+    save_figs_to_zip(
+        [("a", fig), ("b", fig)],
+        path = "c.zip",
+        fmt = "png",
+        overwrite = False
+    )
+    assert os.path.exists("c.zip")
+    with ZipFile("c.zip") as zfd:
+        print(zfd.namelist())
+        fd = zfd.open("a.png")
+    fd.close()
+
+    save_figs_to_zip(
+        [("a", fig), ("b", fig)],
+        path = "d/e.zip",
+        fmt = "png",
+        overwrite = False
+    )
+    assert os.path.exists("d/e.zip")
+    with ZipFile("d/e.zip") as zfd:
+        print(zfd.namelist())
+        fd = zfd.open("a.png")
+    fd.close()
+
+def test_save_figs_to_zip_overwrite(tmp_path):
+    # overwrite tests
+    fig, _ = plt.subplots()
+    os.chdir(tmp_path)
+
+    # touch "c.zip"
+    open("c.zip", "w").close()
+
+    with pytest.raises(FileExistsError):
+        save_figs_to_zip(
+            [("a", fig), ("b", fig)],
+            path = "c.zip",
+            fmt = "png",
+            overwrite = False
+        )
+
+    save_figs_to_zip(
+        [("a", fig), ("b", fig)],
+        path = "c.zip",
+        fmt = "png",
+        overwrite = True
+    )
+    assert os.path.exists("c.zip")
+    with ZipFile("c.zip") as zfd:
+        print(zfd.namelist())
+        fd = zfd.open("a.png")
+    fd.close()
+
+def test_bbox():
+    a = [[-1,-1], [0,-1], [-1,0]]
+    b = [[1,0.5], [-0.5,1]]
+    c = bounding_bbox(a,b)
+    assert c.x0 == -1 and c.x1 == 1 and c.y0 == -1 and c.y1 == 1
+
+def test_plot_artists_in_view():
+    c = Bbox.from_extents(-1,-1,1,1)
+    fig, ax = plot_artists_in_view(bbox=c, margin=0.1)
+    b = ax.viewLim
+    assert b.x0 == -1.2 and b.x1 == 1.2 and b.y0 == -1.2 and b.y1 == 1.2
+    assert ax.get_aspect() == 1
--- tope/plot.py
+++ tope/plot.py
@@ -2,9 +2,11 @@ import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
 from matplotlib.text import Text, Annotation
 
-import os, shutil, tempfile
+import os
 from typing import Iterable
-
+from zipfile import ZipFile
+from tempfile import mkdtemp
+from shutil import rmtree
 
 def save_figs_to_dir(
     figs: Iterable, 
@@ -25,25 +27,71 @@ def save_figs_to_dir(
         fig.savefig(fname, dpi=dpi) 
         # default behaviour is to overwrite
         # if we made it this far, either force = True or the directory didn't exist
-            
+
+def copy_dir_to_zip(
+    path: str,
+    directory: str
+):
+    """
+    Copy contents of directory (non-recursively) to a zip archive.
+    """
+    with ZipFile(path, "w") as zfd:
+        for fname in os.listdir(directory):
+            zfd.write(os.path.join(directory, fname), arcname=fname)
+    # to make recursive use os.walk()
+
 def save_figs_to_zip(
     figs: Iterable, 
-    arcname: str,
+    path: str,
     dpi: int = 300,
-    force: bool = False, # force => parents
-    as_zip: bool = True,
+    overwrite: bool = False,
     fmt: str = "png"
 ):
     # make directory
-    import tempfile
-    tmpdir = tempfile.mkdtemp()
-    save_figs_to_dir(figs, dpi, tmpdir, parents, overwrite)
+    tmpdir = mkdtemp()
+    save_figs_to_dir(
+        figs, 
+        dpi=dpi, 
+        directory=tmpdir, 
+        force=True, # tmpdir already exists
+        fmt=fmt
+    )
 
     # compress and add to archive
-    from zipfile import ZipFile
-    with ZipFile(arcname, 'w') as zfd:
-        for fname in os.listdir(tmppath):
-            zfd.write(fname)
-        
-    from shutil import rmtree
-    rmtree(tmppath)
+    if os.path.exists(path):
+        if not overwrite:
+            raise FileExistsError("Path already exists.")
+        if overwrite and os.path.isdir(path):
+            logger.warning(f"Warning: removing directory tree at {path}.")
+            rmtree(path)
+
+    try:
+        copy_dir_to_zip(path, tmpdir)
+    except FileNotFoundError: # parents of path don't exist
+        os.makedirs(os.path.dirname(path))
+        copy_dir_to_zip(path, tmpdir)
+    rmtree(tmpdir)
+
+from matplotlib.transforms import Bbox
+
+def bounding_bbox(*arrays): # arrays
+    box = Bbox.null()
+    for array in arrays:
+        box.update_from_data_xy(array, ignore=False)
+    return box
+
+def plot_artists_in_view(
+    *artists,
+    bbox: Bbox = Bbox.null(),
+    margin: float = 0.05
+):
+    fig, ax = plt.subplots()
+    for ar in artists:
+        ax.add_artist(ar)
+    ax.dataLim = bbox
+    ax.set_aspect(1)
+    ax.set_xmargin(margin)
+    ax.set_ymargin(margin)
+    ax.autoscale()
+    return fig, ax
+
